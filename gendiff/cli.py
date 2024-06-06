2ebf806d08e428e1d744ed54f84a5369b8fbad8c
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-06-06 11:18:04 +0500

    Add cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,19 +1,9 @@
-#!/usr/bin/env python3
-from gendiff.cli import parsing_args1
 import argparse
 
 
-def parsing_args():
+def parsing_args1():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-
-
-def main():
-    parsing_args1()
-
-
-if __name__ == '__main__':
-    main()
