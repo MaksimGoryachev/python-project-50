7eb5cbafc981c52e7d6124c5376a45d3df5e3f35
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-06-23 16:26:37 +0500

    Add gendiff/formatters/__init__.py

---
+++ gendiff/formatters/__init__.py
@@ -0,0 +1,7 @@
+"""
+This is a formatters package.
+"""
+
+from gendiff.formatters.stylish import formatter_stylish as stylish
+from gendiff.formatters.plain import formatter_plain as plain
+from gendiff.formatters.json import formatter_json as json
