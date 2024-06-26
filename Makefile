e0cbfbe3bb0068fb33149f63273eaaca0a70b1d1
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-06-26 12:35:34 +0500

    Fix test_gendiff.py, update Makefile

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ package-install:
 	python3 -m pip install dist/*.whl
 
 lint:
-	poetry run flake8 gendiff
+	poetry run flake8 gendiff tests
 
 test:
 	poetry run pytest
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -10,6 +10,7 @@ def read_file(path: Path) -> str:
     with open(path, 'r', encoding="utf-8") as file:
         return file.read().strip()
 
+
 @pytest.mark.parametrize("path", [Path('./tests/fixtures')])
 def test_run_gendiff(path: Path):
     """
@@ -17,7 +18,7 @@ def test_run_gendiff(path: Path):
     """
     got_json = generate_diff(path / 'file1.json', path / 'file2.json')
     got_yaml = generate_diff(path / 'file1.yaml', path / 'file2.yaml')
-    expected = read_file(path /'res_stylish')
+    expected = read_file(path / 'res_stylish')
     assert got_json == expected
     assert got_yaml == expected
 
@@ -25,7 +26,7 @@ def test_run_gendiff(path: Path):
                                     path / 'file_nested2.json', 'stylish')
     got_nested_yaml = generate_diff(path / 'file_nested1.yaml',
                                     path / 'file_nested2.yaml', 'stylish')
-    expected_nested = read_file(path /'res_stylish_nested')
+    expected_nested = read_file(path / 'res_stylish_nested')
     assert got_nested_json == expected_nested
     assert got_nested_yaml == expected_nested
 
@@ -37,7 +38,7 @@ def test_run_gendiff(path: Path):
     got_json_nested = generate_diff(path / 'file_nested1.json',
                                     path / 'file_nested2.json', 'json')
 
-    expected_json = read_file(path /'res_json_nested')
+    expected_json = read_file(path / 'res_json_nested')
     assert got_json_nested == expected_json
 
 # @pytest.mark.parametrize('first_file, second_file, expected', [
@@ -46,9 +47,7 @@ def test_run_gendiff(path: Path):
 #     ('./tests/fixtures/file1.yaml', './tests/fixtures/file2.yaml',
 #      './tests/fixtures/res_stylish')])
 # def test_run_gendiff(first_file, second_file, expected):
-#     """
-#     This function tests the `generate_diff` function with different input files.
-#     """
+
 #     got_generate_diff = generate_diff(first_file, second_file)
 #     with open(expected, 'r', encoding="utf-8") as f:
 #         expected = f.read()
