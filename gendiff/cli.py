b4c96c4b8bf961444bfba8b9d6a48c06fa185efc
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-06-26 18:00:23 +0500

    Update diff.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -17,5 +17,4 @@ def parsing_args():
                         choices=['stylish', 'plain', 'json'],
                         default='stylish',
                         help='set format of output', type=str)
-    # args = parser.parse_args()
-    return parser.parse_args()  # args.first_file, args.second_file, args.format
+    return parser.parse_args()
